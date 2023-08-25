bd86b80908cc87506cb61cbeb022a0766f92931c
Brandon Lotero <blotero@gmail.com>
2023-08-25 03:54:50 -0500

    Minor adjustment in battery measurement

--- dwmblocks/scripts/battery.sh
+++ dwmblocks/scripts/battery.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo $(cat /sys/class/power_supply/BAT0/capacity)  "%"
+echo "scale=2 ; $(cat /sys/class/power_supply/BAT1/energy_now ) / $(cat /sys/class/power_supply/BAT1/energy_full ) * 100" | bc
